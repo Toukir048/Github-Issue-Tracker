c4a7bc16cb88cb72c4129c4c57f15148defcf12b
toukir048 <anjumtanvir117@gmail.com>
2026-03-15 10:05:06 -0700

    Optimize the Tab Click Functionality

--- script/tabs.js
+++ script/tabs.js
@@ -1,13 +1,13 @@
 const tabs = document.querySelectorAll(".tab-btn");
 
 tabs.forEach(tab => {
-    tab.addEventListener("click", function () {
+    tab.addEventListener("click", () => {
 
         tabs.forEach(t => {
             t.classList.remove("bg-blue-800", "text-white");
         });
 
-        this.classList.add("bg-blue-800", "text-white");
+        tab.classList.add("bg-blue-800", "text-white");
 
     });
 });
